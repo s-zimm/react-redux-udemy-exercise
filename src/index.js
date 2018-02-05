ff2554929a3ef5be8f06506b572f60de99fe7179
Seth <s.zim517@gmail.com>
2018-02-05 10:24:56 -0500

    Adds youtube search api module and sets up search bar component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBH1rf5lgJuNwltVbWSfVXN-vGmZJZRB3w';
+
 // Create a new component.
 // This component should produce HTML
 
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 // Take this component's generated HTML and put it in the DOM
